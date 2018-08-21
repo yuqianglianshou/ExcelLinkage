9d90c7dd018c13f22a04f119ad68e82f38a82f27
{liuqingyang} <{2465199797@qq.com}>
2018-08-21 12:34:17 +0800

    添加解释说明

--- app/src/main/java/com/lanqi/excellinkage/InterceptLinearLayout.java
+++ app/src/main/java/com/lanqi/excellinkage/InterceptLinearLayout.java
@@ -2,7 +2,6 @@ package com.lanqi.excellinkage;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
 
@@ -13,33 +12,21 @@ import android.widget.LinearLayout;
  * */
 public class InterceptLinearLayout extends LinearLayout {
 
-	public InterceptLinearLayout(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public InterceptLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public InterceptLinearLayout(Context context) {
-		super(context);
-	}
-//	
-//	@Override
-//	public boolean dispatchTouchEvent(MotionEvent ev) {
-//		//return super.dispatchTouchEvent(ev);
-//		Log.i("pdwy","ScrollContainer dispatchTouchEvent");
-//		return true;
-//	}
+    public InterceptLinearLayout(Context context) {
+        super(context);
+    }
 
-	@Override
-	public boolean onInterceptTouchEvent(MotionEvent ev) {
-		//return super.onInterceptTouchEvent(ev);
-		Log.i("pdwy","ScrollContainer onInterceptTouchEvent");
-		return true;
-		
-		//return super.onInterceptTouchEvent(ev);
-	}
-	
-//	@Override
-//	public boolean onTouchEvent(MotionEvent event) {
-//		Log.i("pdwy","ScrollContainer onTouchEvent");
-//		return true;
-//	}
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        //返回true,则当前LinearLayout拦截了事件，子控件失去了处理事件的能力
+        return true;
+
+        //return super.onInterceptTouchEvent(ev); 这个是控件默认的返回，如果放开，则每个item都可以自己处理内部的事件，效果是每个item都可以单独的滑动。
+    }
+
 }
